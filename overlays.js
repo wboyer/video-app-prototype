0170c4fcf954448a5176850c0d674ac75db18385
Bill Boyer <william.boyer.nyc@gmail.com>
2012-07-15 14:16:23 -0400

    Added support for "covered" overlays.  Still not actually penalizing for covering, though, or layering properly.

--- overlays.js
+++ overlays.js
@@ -13,9 +13,17 @@ Arrangement.construct = function (copyFrom)
 {
 	this.placements = [];
 
-	if (copyFrom != null)
+	if (copyFrom)
 		for (var p = 0; p < copyFrom.placements.length; p++)
 			this.placements[p] = copyFrom.placements[p];
+};
+
+Arrangement.copyAndAddPlacement = function (placement)
+{
+	var arrangement = Object.create(Arrangement);
+	arrangement.construct(this);
+	arrangement.placements[arrangement.placements.length] = placement;
+	return arrangement;
 };
 
 Arrangement.findBest = function (overlays, targetRegion)
@@ -43,23 +51,35 @@ Arrangement.findBest = function (overlays, targetRegion)
 	return arrangements[bestArrangement];
 };
 
-Arrangement.computeScore = function (region)
+Arrangement.computeScore = function (targetRegion)
 {
 	var score = 0;
 	
 	for (var p = 0; p < this.placements.length; p++)
-		score += this.placements[p].computeScore(region);
+		score += this.placements[p].computeScore(targetRegion);
 	
 	return score;
 };
 
 
-var Placement = {overlay: null, region: null, left: 0, top: 0, right: 0, bottom: 0, layer: 0};
+var Placement = {overlay: null, allowedRegion: null, left: 0, top: 0, right: 0, bottom: 0};
 
-Placement.computeScore = function (region)
+Placement.construct = function (overlay, left, top, right, bottom, allowedRegion)
 {
-	var regionMiddle = ((region.bottom - 1 - region.top) / 2) + this.region.bias;
-	var regionCenter = ((region.right - 1 - region.left) / 2) - this.region.bias;
+	var placement = Object.create(Placement);
+	placement.overlay = overlay;
+	placement.left = left;
+	placement.top = top;
+	placement.right = right;
+	placement.bottom = bottom;
+	placement.allowedRegion = allowedRegion;	
+	return placement;
+};
+
+Placement.computeScore = function (targetRegion)
+{
+	var regionMiddle = ((targetRegion.bottom - 1 - targetRegion.top) / 2) + this.allowedRegion.bias;
+	var regionCenter = ((targetRegion.right - 1 - targetRegion.left) / 2) - this.allowedRegion.bias;
 	
 	var middle = (this.bottom - 1 + this.top) / 2;
 	var center = (this.right - 1 + this.left) / 2;
@@ -87,7 +107,7 @@ Region.construct = function (left, top, right, bottom, axis, bias)
 	this.bias = bias;
 };
 
-var Overlay = {id: "none", width: 90, height: 90, style: "overlay", color: "#FFFF00", allowedRegions: null, enabled: true};
+var Overlay = {id: "none", width: 90, height: 90, style: "overlay", color: "#FFFF00", allowedRegions: null, canBeCovered: false, enabled: true};
 
 Overlay.addAllowedRegion = function (region)
 {
@@ -99,33 +119,36 @@ Overlay.addAllowedRegion = function (region)
 
 Overlay.arrange = function (overlays, existingArrangements)
 {
-	var myArrangements = [];
-	var a = 0;			
+	var newArrangements = [];
 
+	if (existingArrangements == null)
+	{
+		var newArrangement = Object.create(Arrangement);
+		newArrangement.construct(null);
+		existingArrangements = [];
+		existingArrangements[0] = newArrangement;
+	}
+	
 	for (var r = 0; r < this.allowedRegions.length; r++)
-		if (existingArrangements != null) {
-			Arrangement.copyArrangements(existingArrangements, myArrangements);
+		for (var a  = 0; a < existingArrangements.length; a++)
+		{
+			var arrangement = existingArrangements[a];
 
-			for (; a < myArrangements.length; a++)
-				this.addLowPlacement(myArrangements[a].placements, this.allowedRegions[r]);
-	
-			Arrangement.copyArrangements(existingArrangements, myArrangements);
+			var newPlacements = this.createLowPlacements(arrangement.placements, this.allowedRegions[r]);
+			if (newPlacements.length > 0)
+				for (var p = 0; p < newPlacements.length; p++)
+					newArrangements[newArrangements.length] = arrangement.copyAndAddPlacement(newPlacements[p]);
+			else
+				newArrangements[newArrangements.length] = arrangement;
+			
+			newPlacements = this.createHighPlacements(arrangement.placements, this.allowedRegions[r]);
+			if (newPlacements.length > 0)
+				for (var p = 0; p < newPlacements.length; p++)
+					newArrangements[newArrangements.length] = arrangement.copyAndAddPlacement(newPlacements[p]);
+			else
+				newArrangements[newArrangements.length] = arrangement;
+		}
 
-			for (; a < myArrangements.length; a++)
-				this.addHighPlacement(myArrangements[a].placements, this.allowedRegions[r]);
-		}
-		else {
-			var myArrangement = Object.create(Arrangement);
-			myArrangement.construct(null);
-			this.addLowPlacement(myArrangement.placements, this.allowedRegions[r]);
-			myArrangements[a++] = myArrangement;
-	
-			myArrangement = Object.create(Arrangement);
-			myArrangement.construct(null);
-			this.addHighPlacement(myArrangement.placements, this.allowedRegions[r]);
-			myArrangements[a++] = myArrangement;
-		}
-		
 	var remainingOverlays = [];
 	for (var i = 0, j = 0; i < overlays.length; i++)
 		if ((this != overlays[i]) && overlays[i].enabled)
@@ -135,26 +158,212 @@ Overlay.arrange = function (overlays, existingArrangements)
 
 	if (remainingOverlays.length > 0)
 		for (var j = 0; j < remainingOverlays.length; j++)
-			Arrangement.copyArrangements(remainingOverlays[j].arrange(remainingOverlays, myArrangements), finalArrangements);
+			Arrangement.copyArrangements(remainingOverlays[j].arrange(remainingOverlays, newArrangements), finalArrangements);
 	else
-		Arrangement.copyArrangements(myArrangements, finalArrangements);
+		Arrangement.copyArrangements(newArrangements, finalArrangements);
 		
 	return finalArrangements;
 };
 
-Overlay.addLowPlacement = function (placements, region)
+Overlay.createLowPlacements = function (existingPlacements, allowedRegion)
+{
+	var newPlacements = [];
+
+	switch (allowedRegion.axis) {
+		case "LCUC":
+			var left = allowedRegion.left + (allowedRegion.right - allowedRegion.left - this.width) / 2;
+			var top = allowedRegion.bottom - this.height;
+			var right = left + this.width;
+			var bottom = allowedRegion.bottom;
+
+			while (true) {
+				var coveredPlacements = null;
+				var cantCoverPlacement = null;
+				var nextBottom = allowedRegion.top;
+				
+				for (var p = 0; p < existingPlacements.length; p++) {
+					var placement = existingPlacements[p];
+					if (placement.overlaps(left, top, right, bottom))
+						if (placement.overlay.canBeCovered) {
+							if (!coveredPlacements)
+								coveredPlacements = [];
+							coveredPlacements[coveredPlacements.length] = placement;
+							if (nextBottom > placement.top)
+								nextBottom = placement.top;
+						}
+						else {
+							cantCoverPlacement = placement;
+							nextBottom = placement.top;
+							break;
+						}
+				}
+
+				if (!cantCoverPlacement) {
+					newPlacements[newPlacements.length] = Placement.construct(this, left, top, right, bottom, allowedRegion, coveredPlacements);
+					if (!coveredPlacements)
+						break;
+				}
+				
+				bottom = nextBottom;
+				top = bottom - this.height;
+				if (top < allowedRegion.top)
+					break;
+			};
+
+			break;
+
+		case "ULUR":
+			var left = allowedRegion.left;
+			var top = allowedRegion.top;
+			var right = left + this.width;
+			var bottom = top + this.height;
+
+			while (true) {
+				var coveredPlacements = null;
+				var cantCoverPlacement = null;
+				var nextLeft = allowedRegion.right;
+				
+				for (var p = 0; p < existingPlacements.length; p++) {
+					var placement = existingPlacements[p];
+					if (placement.overlaps(left, top, right, bottom))
+						if (placement.overlay.canBeCovered) {
+							if (!coveredPlacements)
+								coveredPlacements = [];
+							coveredPlacements[coveredPlacements.length] = placement;
+							if (nextLeft > placement.right)
+								nextLeft = placement.right;
+						}
+						else {
+							cantCoverPlacement = placement;
+							nextLeft = placement.right;
+							break;
+						}
+				}
+
+				if (!cantCoverPlacement) {
+					newPlacements[newPlacements.length] = Placement.construct(this, left, top, right, bottom, allowedRegion, coveredPlacements);
+					if (!coveredPlacements)
+						break;
+				}
+				
+				left = nextLeft;
+				right = left + this.width;
+				if (right > allowedRegion.right)
+					break;
+			};
+			
+			break;
+	};
+	
+	return newPlacements;
+};
+
+Overlay.createHighPlacements = function (existingPlacements, allowedRegion)
+{
+	var newPlacements = [];
+
+	switch (allowedRegion.axis) {
+		case "LCUC":
+			var left = allowedRegion.right - (allowedRegion.right - allowedRegion.left - this.width) / 2 - this.width;
+			var top = allowedRegion.top;
+			var right = left + this.width;
+			var bottom = top + this.height;
+			
+			while (true) {
+				var coveredPlacements = null;
+				var cantCoverPlacement = null;
+				var nextTop = allowedRegion.bottom;
+				
+				for (var p = 0; p < existingPlacements.length; p++) {
+					var placement = existingPlacements[p];
+					if (placement.overlaps(left, top, right, bottom))
+						if (placement.overlay.canBeCovered) {
+							if (!coveredPlacements)
+								coveredPlacements = [];
+							coveredPlacements[coveredPlacements.length] = placement;
+							if (nextTop > placement.bottom)
+								nextTop = placement.bottom;
+						}
+						else {
+							cantCoverPlacement = placement;
+							nextTop = placement.bottom;
+							break;
+						}
+				}
+
+				if (!cantCoverPlacement) {
+					newPlacements[newPlacements.length] = Placement.construct(this, left, top, right, bottom, allowedRegion, coveredPlacements);
+					if (!coveredPlacements)
+						break;
+				}
+				
+				top = nextTop;
+				bottom = top + this.height;
+				if (bottom > allowedRegion.bottom)
+					break;
+			};
+
+			break;
+
+		case "ULUR":
+			var left = allowedRegion.right - this.width;
+			var top = allowedRegion.top;
+			var right = allowedRegion.right;
+			var bottom = top + this.height;
+			
+			while (true) {
+				var coveredPlacements = null;
+				var cantCoverPlacement = null;
+				var nextRight = allowedRegion.left;
+				
+				for (var p = 0; p < existingPlacements.length; p++) {
+					var placement = existingPlacements[p];
+					if (placement.overlaps(left, top, right, bottom))
+						if (placement.overlay.canBeCovered) {
+							if (!coveredPlacements)
+								coveredPlacements = [];
+							coveredPlacements[coveredPlacements.length] = placement;
+							if (nextRight > placement.left)
+								nextRight = placement.left;
+						}
+						else {
+							cantCoverPlacement = placement;
+							nextRight = placement.left;
+							break;
+						}
+				}
+
+				if (!cantCoverPlacement) {
+					newPlacements[newPlacements.length] = Placement.construct(this, left, top, right, bottom, allowedRegion, coveredPlacements);
+					if (!coveredPlacements)
+						break;
+				}
+				
+				right = nextRight;
+				left = right - this.width;
+				if (left < allowedRegion.left)
+					break;
+			};
+			
+			break;
+	};
+	
+	return newPlacements;
+};
+
+Overlay.addLowPlacement = function (placements, allowedRegion)
 {
 	var left = 0;
 	var top = 0;
 	var right = 0;
 	var bottom = 0;
 	
-	switch (region.axis) {
+	switch (allowedRegion.axis) {
 		case "LCUC":
-			left = region.left + (region.right - region.left - this.width) / 2;
-			top = region.bottom - this.height;
+			left = allowedRegion.left + (allowedRegion.right - allowedRegion.left - this.width) / 2;
+			top = allowedRegion.bottom - this.height;
 			right = left + this.width;
-			bottom = region.bottom;
+			bottom = allowedRegion.bottom;
 			
 			while (true) {
 				var p = 0;
@@ -163,20 +372,20 @@ Overlay.addLowPlacement = function (placements, region)
 					if (placement.overlaps(left, top, right, bottom)) {
 						bottom = placement.top;
 						top = bottom - this.height;
-						if (top < region.left)
+						if (top < allowedRegion.left)
 							return;
 						else
 							break;
-					};
-				};
+					}
+				}
 				if (p == placements.length)
 					break;
 			};
 			break;
 	
 		case "ULUR":
-			left = region.left;
-			top = region.top;
+			left = allowedRegion.left;
+			top = allowedRegion.top;
 			right = left + this.width;
 			bottom = top + this.height;
 			
@@ -187,12 +396,12 @@ Overlay.addLowPlacement = function (placements, region)
 					if (placement.overlaps(left, top, right, bottom)) {
 						left = placement.right;
 						right = left + this.width;
-						if (right > region.right)
+						if (right > allowedRegion.right)
 							return;
 						else
 							break;
-					};
-				};
+					}
+				}
 				if (p == placements.length)
 					break;
 			};
@@ -205,22 +414,22 @@ Overlay.addLowPlacement = function (placements, region)
 	placement.top = top;
 	placement.right = right;
 	placement.bottom = bottom;
-	placement.region = region;
+	placement.allowedRegion = allowedRegion;
 	
 	placements[placements.length] = placement;
 };
 
-Overlay.addHighPlacement = function (placements, region)
+Overlay.addHighPlacement = function (placements, allowedRegion)
 {
 	var left = 0;
 	var top = 0;
 	var right = 0;
 	var bottom = 0;
 	
-	switch (region.axis) {
+	switch (allowedRegion.axis) {
 		case "LCUC":
-			left = region.right - (region.right - region.left - this.width) / 2 - this.width;
-			top = region.top;
+			left = allowedRegion.right - (allowedRegion.right - allowedRegion.left - this.width) / 2 - this.width;
+			top = allowedRegion.top;
 			right = left + this.width;
 			bottom = top + this.height;
 			
@@ -231,21 +440,21 @@ Overlay.addHighPlacement = function (placements, region)
 					if (placement.overlaps(left, top, right, bottom)) {
 						top = placement.bottom;
 						bottom = top + this.height;
-						if (bottom > region.bottom)
+						if (bottom > allowedRegion.bottom)
 							return;
 						else
 							break;
-					};
-				};
+					}
+				}
 				if (p == placements.length)
 					break;
 			};
 			break;
 
 		case "ULUR":
-			left = region.right - this.width;
-			top = region.top;
-			right = region.right;
+			left = allowedRegion.right - this.width;
+			top = allowedRegion.top;
+			right = allowedRegion.right;
 			bottom = top + this.height;
 			
 			while (true) {
@@ -255,12 +464,12 @@ Overlay.addHighPlacement = function (placements, region)
 					if (placement.overlaps(left, top, right, bottom)) {
 						right = placement.left;
 						left = right - this.width;
-						if (left < region.left)
+						if (left < allowedRegion.left)
 							return;
 						else
 							break;
-					};
-				};
+					}
+				}
 				if (p == placements.length)
 					break;
 			};
@@ -273,7 +482,7 @@ Overlay.addHighPlacement = function (placements, region)
 	placement.top = top;
 	placement.right = right;
 	placement.bottom = bottom;
-	placement.region = region;
+	placement.allowedRegion = allowedRegion;
 
 	placements[placements.length] = placement;
 };
