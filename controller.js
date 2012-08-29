ac3feb4026f0b5a43439301593350a6c2eae268a
Bill Boyer <william.boyer.nyc@gmail.com>
2012-08-29 09:39:41 -0400

    A bunch of changes to whitespace.  Also added semicolons to make Eclipse warnings go away.
    The only material changes are to skipForward() and skipBackward(), which now support hidden="pre"|"post".

--- controller.js
+++ controller.js
@@ -7,26 +7,22 @@ VIACOM.Schedule.Controller = ( function () {
 
   var Cors = VIACOM.Cors;
 
-  var schedule, viewer, live, clock;
+  var schedule = null, viewer = null, live = null, clock = null;
 
   //schedule.startTime = this.now();
-/*
+  /*
   var clock = new RemoteClock('http://schedule.mtvnservices-d.mtvi.com/api/v1/now.esi', {
     initialTimeUTC: schedule.now,       // get this from the schedule data feed
     maxDriftMsec: 2000,        // this is the default value, so you could leave it out
     updateFrequencyMsec: 1000  // also the default value
   });
-*/
-
-
-
+   */
 
   // Constructor function for the ViewerStatus "class"
   var ViewerStatus = function(spec) {
 
     // Function to zero-out all the state variables
     this.reset = function() {
-      //this.
       this.blockIndex = 0;
       this.itemIndex = 0;
       this.offset = 0;
@@ -34,20 +30,20 @@ VIACOM.Schedule.Controller = ( function () {
       this.wait = 0;
       this.adsEnabled = false;
       this.hasLoopedBlock = false;
-    }
+    };
 
-      if (spec) {
-        this.blockIndex = spec.blockIndex() || 0;
-        this.itemIndex = spec.itemIndex() || 00;
-        this.offset = spec.offset() || 0;
-        this.time = spec.time() || 0;
-        this.wait = spec.wait() || 0;
-        this.adsEnabled = spec.adsEnabled() || false;
-        this.hasLoopedBlock = spec.hasLoopedBlock() || false;
-      }
-      else {
-        this.reset()
-      }
+    if (spec) {
+      this.blockIndex = spec.blockIndex() || 0;
+      this.itemIndex = spec.itemIndex() || 0;
+      this.offset = spec.offset() || 0;
+      this.time = spec.time() || 0;
+      this.wait = spec.wait() || 0;
+      this.adsEnabled = spec.adsEnabled() || false;
+      this.hasLoopedBlock = spec.hasLoopedBlock() || false;
+    }
+    else
+      this.reset();
+
     // Create a read-only "interface" to the internal state
     var ro = {};
 
@@ -63,35 +59,27 @@ VIACOM.Schedule.Controller = ( function () {
       ro[prop] = (function(p) {
         return function() {
           return that[p];
-        }
+        };
       })(prop);
     }
 
     this.readOnlyCopy = ro;
-  }
-
-
-
- 
-
-
+  };
 
   var now = function () {
     // will need to base this on server time eventually.
     //var theTimeIs = new Date().getTime();
-    var theTimeIs = clock.getCurrentTime()
+    var theTimeIs = clock.getCurrentTime();
     //trace("the time is: " + theTimeIs);
     //trace("the clock sez: " + theClockSez);
     return theTimeIs;
     //return theTimeIs;
-  
-  }
+  };
 
   // private
   // Step to a particular point in time
   var stepToTime = function (time, status)
   {
-  
     var timeOffset = time - schedule.startTime;
 
     var b = status.blockIndex;
@@ -106,91 +94,88 @@ VIACOM.Schedule.Controller = ( function () {
     var done = false;
 
     while (!done)
-      {
-        done = true;
+    {
+      done = true;
 
-        // if we've reached the end of the items in a block, we've looped back
-        if (i >= items.length) {
+      // if we've reached the end of the items in a block, we've looped back
+      if (i >= items.length) {
+        i = 0;
+        hasLoopedBlock = true;
+      }
+
+      // If there is at least one more block
+      if (b + 1 < blocks.length) {
+
+        var nextBlock = blocks[b + 1];
+        var timeUntilBlockStart = nextBlock.start * 1000 - timeOffset;
+
+        // If next block is appt block, mssl is overridden
+        var mssl = nextBlock.mssl;
+        if (nextBlock.appt)
+          mssl = 0;
+
+        //total duration = item duration + ad duration
+        var duration = items[i].duration + items[i].adDuration;
+        //if the next item is in the same playlist and it's an auto playlist 
+        //(i.e., the player handles stepping), add up the durations
+        //of the items in that playlist
+        for (var j = i + 1; (j < items.length) && (items[i].playlistUri == items[j].playlistUri) && items[j].auto; j++)
+          duration += items[j].duration + items[j].adDuration;
+
+        // 1. Is the duration of the current block minus the mssl of the next block (i.e., the minimum time to finish the block)
+        // greater than the time until the next block?
+        // 2. Has the the block looped or does the block prohibit finishing early?
+        // -- If both of these are true, then continue to next block
+        if ((mssl >= 0) && ((duration - mssl) * 1000 > timeUntilBlockStart) && (hasLoopedBlock || !block.dfe)) {
+          b += 1;
           i = 0;
-          hasLoopedBlock = true;
-        }
-
-        // If there is at least one more block
-        if (b + 1 < blocks.length) {
-         
-          var nextBlock = blocks[b + 1];
-          var timeUntilBlockStart = nextBlock.start * 1000 - timeOffset;
-
-          // If next block is appt block, mssl is overridden
-          var mssl = nextBlock.mssl;
-          if (nextBlock.appt) {
-            mssl = 0;
-          }
-          //total duration = item duration + ad duration
-          var duration = items[i].duration + items[i].adDuration;
-          //if the next item is in the same playlist and it's an auto playlist 
-          //(i.e., the player handles stepping), add up the durations
-          //of the items in that playlist
-          for (var j = i + 1; (j < items.length) && (items[i].playlistUri == items[j].playlistUri) && items[j].auto; j++) {
-            duration += items[j].duration + items[j].adDuration;
-          }
-          // 1. Is the duration of the current block minus the mssl of the next block (i.e., the minimum time to finish the block)
-          // greater than the time until the next block?
-          // 2. Has the the block looped or does the block prohibit finishing early?
-          // -- If both of these are true, then continue to next block
-          if ((mssl >= 0) && ((duration - mssl) * 1000 > timeUntilBlockStart) && (hasLoopedBlock || !block.dfe)) {
-            b += 1;
-            i = 0;
-            block = blocks[b];
-            hasLoopedBlock = false;
-            done = false;
-          }
+          block = blocks[b];
+          hasLoopedBlock = false;
+          done = false;
         }
       }
+    }
 
-      status.wait = 0;
-      status.offset = 0;
-      status.adsEnabled = true;
+    status.wait = 0;
+    status.offset = 0;
+    status.adsEnabled = true;
 
-      //have we stepped to the next block?
-      if (b != status.blockIndex) {
-        var timeUntilBlockStart = block.start * 1000 - timeOffset;
+    //have we stepped to the next block?
+    if (b != status.blockIndex) {
+      var timeUntilBlockStart = block.start * 1000 - timeOffset;
 
-        var msse = block.msse * 1000;
-        // if it's an appt block, we can't start early
-        if (block.appt) {
-          msse = 0;
-        }
-        // If the time until the next block is greater than msse, wait out the difference
-        if ((msse >= 0) && (timeUntilBlockStart > msse)) {
-          status.wait = timeUntilBlockStart - msse;
-        }
-        status.blockIndex = b;
-        status.hasLoopedBlock = false;
-      }
-      else {
-        if (hasLoopedBlock) {
-          status.hasLoopedBlock = true;
-        }
-      }
-      status.itemIndex = i;
+      var msse = block.msse * 1000;
+      // if it's an appt block, we can't start early
+      if (block.appt)
+        msse = 0;
+
+      // If the time until the next block is greater than msse, wait out the difference
+      if ((msse >= 0) && (timeUntilBlockStart > msse))
+        status.wait = timeUntilBlockStart - msse;
+
+      status.blockIndex = b;
+      status.hasLoopedBlock = false;
+    }
+    else {
+      if (hasLoopedBlock)
+        status.hasLoopedBlock = true;
+    }
+    status.itemIndex = i;
   };
 
-   
+
   // Sync viewerStatus with what is live now if possible
   var sync = function (status, now)
   {
-
     status.reset();
 
-    if ((schedule.blocks.length == 0) || (schedule.blocks[0].items.length == 0)) {
+    if ((schedule.blocks.length == 0) || (schedule.blocks[0].items.length == 0))
       return false;
-    }
 
     // set time to when the first block should start
     var time = schedule.startTime + schedule.blocks[0].start * 1000;
-    while (true) {
 
+    while (true) {
       //step forward to the begining
       stepToTime(time, status);
 
@@ -215,9 +200,8 @@ VIACOM.Schedule.Controller = ( function () {
           time += duration;
 
           if (time >= now) {
-            if (block.dll || item.dll) {
+            if (block.dll || item.dll)
               time -= duration;
-            }
             else {
               status.offset = now + duration - time;
               status.adsEnabled = false;
@@ -230,12 +214,10 @@ VIACOM.Schedule.Controller = ( function () {
           }
 
           var i = status.itemIndex + 1;
-          if ((i < block.items.length) && (item.playlistUri == block.items[i].playlistUri) && block.items[i].auto) {
+          if ((i < block.items.length) && (item.playlistUri == block.items[i].playlistUri) && block.items[i].auto)
             status.itemIndex = i;
-          }
-          else {
+          else
             break;
-          }
         }
       }
 
@@ -244,30 +226,26 @@ VIACOM.Schedule.Controller = ( function () {
     return true;
   };
 
- 
+
   var goLive = function () {
     trace("go live");
     var vs = sync(viewer, this.now());
     fire('Live', vs); 
     return vs;
-  }
-
+  };
 
 
   var step = function (status, playerCanStepThroughPlaylist)
   {
-
     trace('step (internal)');
-
 
     var i = status.itemIndex + 1;
 
     var items = schedule.blocks[status.blockIndex].items;
 
     if (playerCanStepThroughPlaylist) {
-      while ((i < items.length) && (items[status.itemIndex].playlistUri == items[i].playlistUri) && items[i].auto) {
+      while ((i < items.length) && (items[status.itemIndex].playlistUri == items[i].playlistUri) && items[i].auto)
         i += 1;
-      }
     }
 
     status.itemIndex = i;
@@ -275,16 +253,16 @@ VIACOM.Schedule.Controller = ( function () {
     return stepToTime(now(), status);
   };
 
-  var stepForward = function (playerCanStepThroughPlaylist) {
+  var stepForward = function (playerCanStepThroughPlaylist)
+  {
     trace('stepForward');
     var vs = step(viewer, playerCanStepThroughPlaylist);
     fire('Step', vs); 
     return vs;
-  }
+  };
 
   var skipForward = function ()
   {
-
     trace('skipForward');
 
     var now = this.now();
@@ -294,31 +272,39 @@ VIACOM.Schedule.Controller = ( function () {
     var blocks = schedule.blocks;
     var block = blocks[b];
     var items = block.items;
-    
+
     if (viewer.wait > 0) {
-      if (block.appt) {
+      if (block.appt)
         i = block.items.length;
-      }
       else {
         viewer.wait = 0;
         return viewer.readOnlyCopy;
       }
     }
     else {
-      i += 1;
-      while ((i < items.length) && items[i].hidden) {
-        i += 1;
+      var playlistUri = items[i].playlistUri;
+      if (items[i].hidden == "playlist")
+        while ((i < items.length) && (items[i].playlistUri == playlistUri) && (items[i].hidden == "playlist"))
+          i += 1;
+      else
+      {
+        while ((i < items.length) && items[i].hidden == "pre")
+          i += 1;
+
+        if ((i < items.length) && !items[i].hidden)
+          i += 1;
+
+        while ((i < items.length) && items[i].hidden == "post")
+          i += 1;
       }
     }
 
     if (i >= items.length) {
       i = 0;
-      if (b + 1 < blocks.length) {
+      if (b + 1 < blocks.length)
         b += 1;
-      }
-      else {
+      else
         b = 0;
-      }
     }
 
     var vs = this.jump(b, i);
@@ -330,9 +316,6 @@ VIACOM.Schedule.Controller = ( function () {
   {
     trace('skipBackward');
 
-    //var schedule = schedule;
-
-
     var b = viewer.blockIndex;
     var i = viewer.itemIndex;
 
@@ -340,40 +323,48 @@ VIACOM.Schedule.Controller = ( function () {
     var block = blocks[b];
     var items = block.items;
 
-    if (viewer.wait > 0) {
-      trace("wait>0");
-      i = -1;
-    }
-    else {
-      i -= 1;
-
-      while ((i >= 0) && items[i].hidden) {
+    var playlistUri = items[i].playlistUri;
+    if (items[i].hidden == "playlist")
+      while ((i >= 0) && (items[i].playlistUri == playlistUri) && (items[i].hidden == "playlist"))
         i -= 1;
-      }
+    else {
+      while ((i >= 0) && items[i].hidden == "post")
+        i -= 1;
+
+      if ((i >= 0) && !items[i].hidden)
+        i -= 1;
+
+      while ((i >= 0) && items[i].hidden == "pre")
+        i -= 1;
     }
 
     if (i < 0) {
-      if (b > 0) {
+      if (b > 0)
         b -= 1;
-      }
-      else {
+      else
         b = blocks.length - 1;
-      }
 
       block = blocks[b];
       items = block.items;
       i = items.length - 1;
-
-      while ((i >= 0) && items[i].hidden) {
-        i -= 1;
-      }
     }
 
-      var vs = this.jump(b, i);
-      fire('SkipBackward', vs);
-      return vs;
+    var playlistUri = items[i].playlistUri;
+    if (items[i].hidden == "playlist")
+      while ((i > 0) && (items[i-1].playlistUri == playlistUri) && (items[i-1].hidden == "playlist"))
+        i -= 1;
+
+    while ((i >= 0) && items[i].hidden == "post")
+      i -= 1;
+
+    while ((i > 0) && (items[i-1].hidden == "pre"))
+      i -= 1;
+    
+    var vs = this.jump(b, i);
+    fire('SkipBackward', vs);
+    return vs;
   };
-  
+
 
   var jump = function (blockIndex, itemIndex)
   {
@@ -381,13 +372,10 @@ VIACOM.Schedule.Controller = ( function () {
 
     var now = this.now();
 
-    //var schedule = schedule;
-
     block = schedule.blocks[blockIndex];
 
-    if (blockIndex != viewer.blockIndex) {
+    if (blockIndex != viewer.blockIndex)
       viewer.hasLoopedBlock = false;
-    }
 
     viewer.wait = 0;
     viewer.offset = 0;
@@ -405,7 +393,6 @@ VIACOM.Schedule.Controller = ( function () {
     viewer.blockIndex = blockIndex;
     viewer.itemIndex = itemIndex;
 
-      
     return viewer.readOnlyCopy;
 
   };
@@ -417,13 +404,12 @@ VIACOM.Schedule.Controller = ( function () {
     var items = schedule.blocks[viewer.blockIndex].items;
 
     for (var i = viewer.itemIndex; (i < items.length) && items[i].auto; i++)
-    if (items[i].videoUri == uri) {
-      viewer.itemIndex = i;
-      break;
-    }
-     
-    return viewer.readOnlyCopy;
+      if (items[i].videoUri == uri) {
+        viewer.itemIndex = i;
+        break;
+      }
 
+    return viewer.readOnlyCopy;
   };
 
   var play = function (player)
@@ -443,67 +429,67 @@ VIACOM.Schedule.Controller = ( function () {
         player.loadPlaylist(playlistUri);
         player.seekToPlaylistVideo(uri, duration);
       }
-      else {
+      else
         player.loadVideo(uri, duration);
-      }
     }
     else {
       player.config(uri);
       player.loadVideo(uri, duration);
     }
 
-    if (viewer.adsEnabled) {
+    if (viewer.adsEnabled)
       player.setAdDuration(adDuration);
-    }
 
-    if (viewer.offset > 0) {
+    if (viewer.offset > 0)
       player.seekToOffset(viewer.offset);
-    }
+
     player.play();
 
-     return viewer.readOnlyCopy;
+    return viewer.readOnlyCopy;
   };
 
-  var timeUntilBlockStart = function (b) {
-
+  var timeUntilBlockStart = function (b)
+  {
     var now = this.now();
     return schedule.startTime + schedule.blocks[b].start * 1000 - now;
   };
 
   var getViewerStatus = function () {
-     return viewer.readOnlyCopy;
-  }
+    return viewer.readOnlyCopy;
+  };
 
   var getLiveStatus = function () {
     //trace("getLiveStatus");
     //live = new ViewerStatus(viewer.readOnlyCopy);
     sync(live, this.now());
     return live.readOnlyCopy;
-  }
+  };
 
   var currentItem = function () {
     return schedule.blocks[viewer.blockIndex].items[viewer.itemIndex];
-  }
+  };
+
   var nextUpItem = function() {
     var next = new ViewerStatus(viewer.readOnlyCopy);
     step(next, true);
     return schedule.blocks[next.blockIndex].items[next.itemIndex];
-  }
+  };
+
   var currentLiveItem = function () {
     return schedule.blocks[live.blockIndex].items[live.itemIndex];
-  }
+  };
 
   var setWait = function(secs) {
     //live.wait = secs;
     viewer.wait = secs;
-  }
+  };
 
 
   // Data structure to store event listeners. Key is event name, value is an array
   // of listeners.
   var eventRegistry = {};
 
-  // Public method to regsiter for the specified eventName. The callback is the function
+  // Public method to register for the specified eventName. The callback is the function
   // to invoke when the event is fired, and the scope determines the value of "this"
   // within the callback function. If no scope is specified, it will default to the
   // global scope (ie, the window). You can register as many listeners as you want for
@@ -545,10 +531,10 @@ VIACOM.Schedule.Controller = ( function () {
 
 
   var setup  = function(options) {
-    
+
     viewer = new ViewerStatus();
     live = new ViewerStatus();
- 
+
     clock = new RemoteClock('http://schedule.mtvnservices-d.mtvi.com/api/v1/now.esi', {
       maxDriftMsec: 2000,
       updateFrequencyMsec: 1000,
@@ -557,20 +543,21 @@ VIACOM.Schedule.Controller = ( function () {
         loadSchedule(function () {
           // Just for testing, compute our own current time,
           // and slide the test program forward to be closer to now.
-         // trace("adjust schedule");
-         // while (schedule.startTime + 3600000 < now()) {
-         //   schedule.startTime += 3600000;
-         // }
+          // trace("adjust schedule");
+          // while (schedule.startTime + 3600000 < now()) {
+          //   schedule.startTime += 3600000;
+          // }
           fire("Ready"); 
-        })
+        });
       }    
     });
-  }
+  };
 
   var setSchedule = function(theSchedule) {
     trace("setSchedule called: " + theSchedule.now);
     schedule = theSchedule;
-  }
+  };
+  
   // TODO add channel paramter
   var loadSchedule = function(callback) {
     Cors.get('http://plateng.mtvi.com/apsv/scheduler/feeds/example.php', {
@@ -579,8 +566,6 @@ VIACOM.Schedule.Controller = ( function () {
         setSchedule(response);
 
         schedule.apptBlocks = [];
-
-   
 
         for (var b = 0; b < schedule.blocks.length; b++) {
           if (schedule.blocks[b].appt) {
@@ -598,15 +583,13 @@ VIACOM.Schedule.Controller = ( function () {
 
   };
 
-
   var playerReady = function() {
-    // whatever we need to do when the player is ready…
-  }
+    // whatever we need to do when the player is ready
+  };
 
   var getSchedule = function () {
     return schedule;
   };
-
 
   return {
     'goLive' : goLive,
@@ -629,8 +612,4 @@ VIACOM.Schedule.Controller = ( function () {
     'getSchedule' : getSchedule
   };
 
-
 }());
-
-
-
